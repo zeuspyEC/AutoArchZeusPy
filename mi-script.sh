1c5ddd99991c3d344ffb71634e6531f58aa13e0d
Erick <139416473+zeuspyEC@users.noreply.github.com>
2024-11-02 02:55:07 -0500

    Update mi-script.sh
    Version 2

--- mi-script.sh
+++ mi-script.sh
@@ -10,67 +10,38 @@ format_center_literals() {
 
 # Banner "ZeusPy"
 display_banner() {
-  local banner=()
-  format_center_literals " ▒███████▒▓█████  █    ██   ██████  ██▓███  ▓██   ██▓"
-  banner+=("$FormatCenterLiterals")
-  format_center_literals "▒ ▒ ▒ ▄▀░▓█   ▀  ██  ▓██▒▒██    ▒ ▓██░  ██▒ ▒██  ██▒"
-  banner+=("$FormatCenterLiterals")
-  format_center_literals "░ ▒ ▄▀▒░ ▒███   ▓██  ▒██░░ ▓██▄   ▓██░ ██▓▒  ▒██ ██░"
-  banner+=("$FormatCenterLiterals")
-  format_center_literals "  ▄▀▒   ░▒▓█  ▄ ▓▓█  ░██░  ▒   ██▒▒██▄█▓▒ ▒  ░ ▐██▓░"
-  banner+=("$FormatCenterLiterals")
-  format_center_literals "▒███████▒░▒████▒▒▒█████▓ ▒██████▒▒▒██▒ ░  ░  ░ ██▒▓░"
-  banner+=("$FormatCenterLiterals")
-  format_center_literals "░▒▒ ▓░▒░▒░░ ▒░ ░░▒▓▒ ▒ ▒ ▒ ▒▓▒ ▒ ░▒▓▒░ ░  ░   ██▒▒▒ "
-  banner+=("$FormatCenterLiterals")
-  format_center_literals "░░▒ ▒ ░ ▒ ░ ░  ░░░▒░ ░ ░ ░ ░▒  ░ ░░▒ ░     ▓██ ░▒░ "
-  banner+=("$FormatCenterLiterals")
-  format_center_literals "░ ░ ░ ░ ░   ░    ░░░ ░ ░ ░  ░  ░  ░░       ▒ ▒ ░░  "
-  banner+=("$FormatCenterLiterals")
-  format_center_literals "  ░ ░       ░  ░   ░           ░           ░ ░     "
-  banner+=("$FormatCenterLiterals")
-  format_center_literals "░                                         ░ ░     "
-  banner+=("$FormatCenterLiterals")
+  local banner=(
+    " ▒███████▒▓█████  █    ██   ██████  ██▓███  ▓██   ██▓"
+    "▒ ▒ ▒ ▄▀░▓█   ▀  ██  ▓██▒▒██    ▒ ▓██░  ██▒ ▒██  ██▒"
+    "░ ▒ ▄▀▒░ ▒███   ▓██  ▒██░░ ▓██▄   ▓██░ ██▓▒  ▒██ ██░"
+    "  ▄▀▒   ░▒▓█  ▄ ▓▓█  ░██░  ▒   ██▒▒██▄█▓▒ ▒  ░ ▐██▓░"
+    "▒███████▒░▒████▒▒▒█████▓ ▒██████▒▒▒██▒ ░  ░  ░ ██▒▓░"
+    "░▒▒ ▓░▒░▒░░ ▒░ ░░▒▓▒ ▒ ▒ ▒ ▒▓▒ ▒ ░▒▓▒░ ░  ░   ██▒▒▒ "
+    "░░▒ ▒ ░ ▒ ░ ░  ░░░▒░ ░ ░ ░ ░▒  ░ ░░▒ ░     ▓██ ░▒░ "
+    "░ ░ ░ ░ ░   ░    ░░░ ░ ░ ░  ░  ░  ░░       ▒ ▒ ░░  "
+    "  ░ ░       ░  ░   ░           ░           ░ ░     "
+    "░                                         ░ ░     "
+  )
 
   clear
   echo -e "\033[34m"
   for line in "${banner[@]}"; do
-    echo "$line"; sleep 0.05
+    format_center_literals "$line"
+    sleep 0.05
   done
-  echo # No eliminar
+  echo
 }
 
 # Función para descargar y ejecutar el script desde GitHub
 download_and_run_script() {
   echo "Descargando y ejecutando el script desde GitHub..."
 
-  # Método 1: Usando curl
-  curl -L https://raw.githubusercontent.com/zeuspyEC/autoArchZ/main/mi-script.sh > mi-script.sh
-  sh mi-script.sh
+  # Descargar el script usando curl
+  curl -s -L https://raw.githubusercontent.com/zeuspyEC/autoArchZ/main/mi-script.sh -o mi-script.sh
 
-  # Método 2: Usando wget
-  # pacman -Sy wget --noconfirm
-  # wget https://raw.githubusercontent.com/zeuspyEC/autoArchZ/main/mi-script.sh
-  # sh mi-script.sh
-
-  # Método 3: Usando curl con un acortador de URL
-  # curl -L is.gd/mi-script > mi-script.sh
-  # sh mi-script.sh
-
-  # Método 4: Clonando el repositorio con git
-  # pacman -Sy git --noconfirm
-  # git clone https://github.com/zeuspyEC/autoArchZ
-  # cd autoArchZ
-  # sh mi-script.sh
-
-  # Método 5: Usando wget y ejecutando con permisos
-  # pacman -Sy wget --noconfirm
-  # wget https://raw.githubusercontent.com/zeuspyEC/autoArchZ/main/mi-script.sh
-  # chmod +x mi-script.sh
-  # ./mi-script.sh
+  # Ejecutar el script
+  bash mi-script.sh
 }
-
-# ...
 
 # Detectar UEFI o BIOS
 detect_boot_mode() {
@@ -96,7 +67,7 @@ detect_windows_installation() {
 
 # Mostrar particiones y solicitar al usuario seleccionar una para la instalación
 select_installation_partition() {
-  partitions=$(lsblk -l -o NAME,SIZE,TYPE | grep 'part\|lvm' | awk '{print $1, $2, $3}')
+  partitions=$(lsblk -n -o NAME,SIZE,TYPE -p | awk '/part|lvm/ {print $1, $2, $3}')
 
   partition_list=()
   while IFS= read -r line; do
@@ -120,36 +91,36 @@ select_installation_partition() {
 
 # Particionar el disco (UEFI)
 partition_disk_uefi() {
-  parted -s /dev/$selected_partition mklabel gpt
-  parted -s /dev/$selected_partition mkpart primary fat32 1 512M
-  parted -s /dev/$selected_partition set 1 esp on
-  parted -s /dev/$selected_partition mkpart primary ext4 512M 100%
-  mkfs.fat -F32 /dev/${selected_partition}1
-  mkfs.ext4 /dev/${selected_partition}2
+  parted -s "$selected_partition" mklabel gpt
+  parted -s "$selected_partition" mkpart primary fat32 1 512M
+  parted -s "$selected_partition" set 1 esp on
+  parted -s "$selected_partition" mkpart primary ext4 512M 100%
+  mkfs.fat -F32 "${selected_partition}1"
+  mkfs.ext4 "${selected_partition}2"
 }
 
 # Particionar el disco (BIOS)
 partition_disk_bios() {
-  parted -s /dev/$selected_partition mklabel msdos
-  parted -s /dev/$selected_partition mkpart primary ext4 1 512M
-  parted -s /dev/$selected_partition set 1 boot on
-  parted -s /dev/$selected_partition mkpart primary ext4 512M 100%
-  mkfs.ext4 /dev/${selected_partition}1
-  mkfs.ext4 /dev/${selected_partition}2
+  parted -s "$selected_partition" mklabel msdos
+  parted -s "$selected_partition" mkpart primary ext4 1 512M
+  parted -s "$selected_partition" set 1 boot on
+  parted -s "$selected_partition" mkpart primary ext4 512M 100%
+  mkfs.ext4 "${selected_partition}1"
+  mkfs.ext4 "${selected_partition}2"
 }
 
 # Montar particiones (UEFI)
 mount_partitions_uefi() {
-  mount /dev/${selected_partition}2 /mnt
+  mount "${selected_partition}2" /mnt
   mkdir -p /mnt/boot/efi
-  mount /dev/${selected_partition}1 /mnt/boot/efi
+  mount "${selected_partition}1" /mnt/boot/efi
 }
 
 # Montar particiones (BIOS)
 mount_partitions_bios() {
-  mount /dev/${selected_partition}2 /mnt
+  mount "${selected_partition}2" /mnt
   mkdir -p /mnt/boot
-  mount /dev/${selected_partition}1 /mnt/boot
+  mount "${selected_partition}1" /mnt/boot
 }
 
 # Instalar sistema base
@@ -182,7 +153,7 @@ EOF
 # Instalar y configurar GRUB (UEFI)
 install_grub_uefi() {
   arch-chroot /mnt /bin/bash <<EOF
-  pacman -S grub efibootmgr
+  pacman -S --noconfirm grub efibootmgr
   grub-install --target=x86_64-efi --efi-directory=/boot/efi --bootloader-id=GRUB
   grub-mkconfig -o /boot/grub/grub.cfg
 EOF
@@ -191,8 +162,8 @@ EOF
 # Instalar y configurar GRUB (BIOS)
 install_grub_bios() {
   arch-chroot /mnt /bin/bash <<EOF
-  pacman -S grub
-  grub-install /dev/$selected_partition
+  pacman -S --noconfirm grub
+  grub-install "$selected_partition"
   grub-mkconfig -o /boot/grub/grub.cfg
 EOF
 }
@@ -201,7 +172,7 @@ EOF
 configure_dual_boot_uefi() {
   if [ "$windows_installed" = true ]; then
     arch-chroot /mnt /bin/bash <<EOF
-    pacman -S os-prober ntfs-3g
+    pacman -S --noconfirm os-prober ntfs-3g
     os-prober
     grub-mkconfig -o /boot/grub/grub.cfg
 EOF
@@ -212,7 +183,7 @@ EOF
 configure_dual_boot_bios() {
   if [ "$windows_installed" = true ]; then
     arch-chroot /mnt /bin/bash <<EOF
-    pacman -S os-prober ntfs-3g
+    pacman -S --noconfirm os-prober ntfs-3g
     os-prober
     grub-mkconfig -o /boot/grub/grub.cfg
 EOF
@@ -222,7 +193,7 @@ EOF
 # Instalar y configurar window manager
 install_window_manager() {
   arch-chroot /mnt /bin/bash <<EOF
-  pacman -S xorg-server xorg-xinit bspwm sxhkd
+  pacman -S --noconfirm xorg-server xorg-xinit bspwm sxhkd
   echo "exec bspwm" > ~/.xinitrc
 EOF
 }
